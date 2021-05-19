ebf1827c5255628e783e5737167d7ba8923e7476
Morten Bertz <morten@telethon.jp>
2021-05-19 20:44:58 +0900

    Initial Commit

---
+++ Shared/SignalGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalGeneratorApp.swift
+//  Shared
+//
+//  Created by Morten Bertz on 2021/05/19.
+//
+
+import SwiftUI
+
+@main
+struct SignalGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
